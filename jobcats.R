136f7aa0bb9d4b51df97ba76097131fde9f000d7
John-o Kulas <kulasj@montclair.edu>
2021-11-02 16:08:34 -0400

    Kulas and Alicia ratings (Kappa=.741)

--- jobcats.R
+++ jobcats.R
@@ -6,3 +6,12 @@ library(ggplot2)
 
 ggplot(data, aes(O.Net.Job.Family.Categories))+
   geom_histogram(stat="count") + coord_flip()
+
+## Using Fleiss' Kappa; https://www.datanovia.com/en/lessons/fleiss-kappa-in-r-for-multiple-categorical-variables/
+
+
+interrater <- read.csv("interrater.csv")
+
+library(irr)
+
+kappam.fleiss(interrater)  
